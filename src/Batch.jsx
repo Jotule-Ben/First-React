7faf302c3aaf67a62446d8c65492289dffdc53f3
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-06-04 12:31:30 +0100

    create and style nav and batch components

---
+++ src/Batch.jsx
@@ -0,0 +1,15 @@
+// creating components
+function Batch() {
+  // logic
+  const a = 5;
+  const b = 5;
+  const c = a * b;
+  return (
+    <div>
+      <h1>Batch component</h1>
+      <p>{c}</p>
+    </div>
+  );
+}
+
+export default Batch;
